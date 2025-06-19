56b6432656dec17f3e6af9468b9f84cfded8dc85
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-06-19 13:45:38 +0200

    working on new implementation on the computer

--- CoinsNClout/Assets/Scripts/UI stuff/UIManager.cs
+++ CoinsNClout/Assets/Scripts/UI stuff/UIManager.cs
@@ -42,7 +42,16 @@ public class UIManager : MonoBehaviour
         foreach(var coin in market.coins)
         {
             GameObject row = Instantiate(coinRowPrefab, contentParent);
-            row.GetComponentInChildren<TMP_Text>().text = $"{coin.Name}:${coin.currentValue:F2}";
+            TMP_Text text = row.transform.Find("CoinText").GetComponent<TMP_Text>();
+            text.text = $"{coin.Name}: ${coin.currentValue:F2}";
+
+            //Buy buttons
+            Button BuyBtn = row.transform.Find("BuyButton").GetComponent<Button>();
+            BuyBtn.onClick.AddListener(() => OnBuyCoin(coin));
+            //Sell button
+            Button sellBtn = row.transform.Find("SellButton").GetComponent<Button>();
+            sellBtn.onClick.AddListener(() => OnSellCoin(coin));
+
             currentRows.Add(row);
         }
     }
@@ -52,5 +61,16 @@ public class UIManager : MonoBehaviour
         {
             RefreshDisplay();
         }
+    }
+
+    private void OnBuyCoin(CryptoCurrency coin)
+    {
+        Debug.Log($"BUY {coin.Name} at ${coin.currentValue:F2}");
+        //implement buy logic here
+    }
+    private void OnSellCoin(CryptoCurrency coin)
+    {
+        Debug.Log($"SELL {coin.Name} at ${coin.currentValue:F2}");
+        //implement sell logic here
     }
 }
